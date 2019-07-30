40feeb2c9ad5fbd607bd589600007399b8981b8b
Alice Duff <duff.alice@gmail.com>
2019-07-30 01:29:36 +0000

    image blur 2

--- image-blur-1.rb
+++ image-blur-1.rb
@@ -10,6 +10,27 @@ attr_accessor :rows
       puts row.inspect
     end
   end
+
+  def image_blur
+
+    newImage = Marshal.load(Marshal.dump(self.rows))
+
+    self.rows.each_index do |y|
+      self.rows[y].each_index do |x|
+        if self.rows[y][x] == 1 then
+          newImage[y-1][x] = 1 if rows[y-1] and rows[y-1][x]
+          newImage[y+1][x] = 1 if rows[y+1] and rows[y+1][x]
+          newImage[y][x-1] = 1 if rows[y][x-1]
+          newImage[y][x+1] = 1 if rows[y][x+1]
+        end
+        # puts "#{x}, #{y}, #{rows[y][x]}"
+      end
+    end
+    newImage.each do |row|
+      puts row.inspect
+    end
+  end
+
 end
 
 image = Image.new(
@@ -21,3 +42,8 @@ image = Image.new(
   ])
 
 image.output_image
+puts "Original Image"
+image.image_blur
+puts "Image - blurred"
+image.output_image
+puts "Original Image"
